87fccbff30cb520b3e6887b9765c06a3c05b6af0
Manny Kharsia <mkharsia@scglobal.ad.scotiacapital.com>
2018-02-18 16:57:40 -0500

    adding files

---
+++ src/com/netcloud/games/slither/Snake.java
@@ -0,0 +1,36 @@
+package com.netcloud.games.slither;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class Snake extends JFrame {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Snake() {
+
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {                
+                JFrame ex = new Snake();
+                ex.setVisible(true);                
+            }
+        });
+    }
+}
